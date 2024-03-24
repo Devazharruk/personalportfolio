f7e1b261dd6e87051fc434794d182c0e228e6e3a
Devazharuk <azharruk66@gmail.com>
2024-03-24 12:04:35 +0500

    Set navbar in for small screens

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const contactForm = document.querySelector(".contact-form");
 const email = document.querySelector("#email");
 const formname = document.querySelector("#name");
 const message = document.querySelector("#message");
+const navLinks = document.querySelectorAll("#nav-link");
 
 navbarAction.addEventListener("click", () => {
     if(navbarAction.classList.contains("bi-list")){
@@ -23,4 +24,11 @@ contactForm.addEventListener("submit", (e) => {
         formname.value="";
         message.value="";
     }, 1000);
-})
+});
+
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        navbarNav.style.left="-100%";
+        navbarAction.classList.replace("bi-x","bi-list");
+    });
+});
