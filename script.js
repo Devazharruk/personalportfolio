d36b6d61a76b01fae9b0627ffdf7769880702679
Devazharuk <azharruk66@gmail.com>
2024-03-24 11:55:18 +0500

    Add formspree for submitting form in the email

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const navbarNav = document.querySelector(".navbar-nav");
 const navbarAction = document.querySelector(".navbar-action i");
+const submitBtn = document.querySelector("#submit-btn");
+const contactForm = document.querySelector(".contact-form");
+const email = document.querySelector("#email");
+const formname = document.querySelector("#name");
+const message = document.querySelector("#message");
 
 navbarAction.addEventListener("click", () => {
     if(navbarAction.classList.contains("bi-list")){
@@ -10,3 +15,12 @@ navbarAction.addEventListener("click", () => {
         navbarAction.classList.replace("bi-x","bi-list")
     }
 });
+
+
+contactForm.addEventListener("submit", (e) => {
+    setTimeout(() => {
+        email.value="";
+        formname.value="";
+        message.value="";
+    }, 1000);
+})
